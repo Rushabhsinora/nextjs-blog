58792c654b0fe88d28d757e49f4ae63104219893
Rushabh <rushabhsinora123@gmail.com>
2023-09-10 17:32:40 +0530

    First Change

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/index.js",
+    "./pages/posts/first-post.js",
+    "./components/layout.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
